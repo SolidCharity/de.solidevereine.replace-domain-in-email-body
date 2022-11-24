7ecf27cd784dca60cf264f3a366f20f76a5f6406
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-11-24 10:13:11 +0100

    run civix upgrade to remove all empty functions

--- replace_domain_in_email_body.php
+++ replace_domain_in_email_body.php
@@ -15,60 +15,6 @@ function replace_domain_in_email_body_civicrm_config(&$config): void {
 }
 
 /**
- * Implements hook_civicrm_install().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
- */
-function replace_domain_in_email_body_civicrm_install(): void {
-  _replace_domain_in_email_body_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function replace_domain_in_email_body_civicrm_postInstall(): void {
-  _replace_domain_in_email_body_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function replace_domain_in_email_body_civicrm_uninstall(): void {
-  _replace_domain_in_email_body_civix_civicrm_uninstall();
-}
-
-/**
- * Implements hook_civicrm_enable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
- */
-function replace_domain_in_email_body_civicrm_enable(): void {
-  _replace_domain_in_email_body_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function replace_domain_in_email_body_civicrm_disable(): void {
-  _replace_domain_in_email_body_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function replace_domain_in_email_body_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _replace_domain_in_email_body_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * Declare entity types provided by this module.
